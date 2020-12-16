5e55025e512bf36503f9d1e655f3982b37d86e9e
Illuminati-CRAZ <42056854+Illuminati-CRAZ@users.noreply.github.com>
2020-12-16 00:54:29 -0800

    parse .qua file

--- QuaMergeDriver/Program.cs
+++ QuaMergeDriver/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using Quaver.API.Maps;
 
 namespace QuaMergeDriver
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string ancestorPath = args[0];
+            string ourPath = args[1];
+            string theirPath = args[2];
+            
+            int blockSize = Int32.Parse(args[3]);
+            
+            int mergeConflict = Merge(ancestorPath, ourPath, theirPath, blockSize);
+            return mergeConflict;
+        }
+        
+        private static int Merge(string ancestorPath, string ourPath, string theirPath, int blockSize = 1000)
+        {
+            // hit objects, timing points, scroll velocities, preview points, editor layers
+            // key counts, diff names
+            // audio file, background file, banner file
+            // map id, mapset id
+            // song title, song artist, song source
+            // tags, creator, description, genre
+            // initial sv, sv mode, scratch key
+            // custom audio samples, sound effects
+            Qua ancestor = Qua.Parse(ancestorPath);
+            Qua ours = Qua.Parse(ourPath);
+            Qua theirs = Qua.Parse(theirPath);
+            
+            int mergeConflict = 0;
+                
+            return mergeConflict;
         }
     }
 }
